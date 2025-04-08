b3d63af9cd1e945348959f3f73a2c2953647a6c0
rhnchowdhury <rhnchdri@gmail.com>
2025-04-08 18:03:40 +0600

    calculator create

---
+++ index.js
@@ -0,0 +1,40 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const app = express();
+const PORT = 4000;
+
+app.use(bodyParser.urlencoded());
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  //   res.send("Server created");
+
+  //! html file send into home page
+  res.sendFile(__dirname + "/Home.html");
+});
+
+app.get("/circle", (req, res) => {
+  res.sendFile(__dirname + "/Circle.html");
+});
+
+app.get("/triangle", (req, res) => {
+  res.sendFile(__dirname + "/Triangle.html");
+});
+
+// ! value calculation
+app.post("/circle", (req, res) => {
+  const radius = req.body.radius;
+  const area = Math.PI * radius * radius;
+  res.send(`<h3>The area of Circle is ${area}</h3>`);
+});
+
+app.post("/triangle", (req, res) => {
+  const height = req.body.height;
+  const base = req.body.base;
+  const area = 0.5 * base * height;
+  res.send(`<h3>The area of Triangle is ${area}</h3>`);
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running at http://localhost:${PORT}`);
+});
